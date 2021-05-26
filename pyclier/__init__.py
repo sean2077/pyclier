4fcb50b2c7b198851d17e3eb26e1c6bdb733eeab
zhangxianbing <zhangxianbing09@163.com>
2021-05-26 19:13:15 +0800

    feat: bump version 0.1.1

--- pyclier/__init__.py
+++ pyclier/__init__.py
@@ -1,6 +1,6 @@
 """Pyclier - A python CLI framework base on argparse, supporting: config system, command-completion, rich-text log and so on."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = """zhangxianbing"""
 __email__ = "zhangxianbing09@163.com"
 
